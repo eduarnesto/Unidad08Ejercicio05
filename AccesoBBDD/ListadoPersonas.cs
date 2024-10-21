c6d85bae1992bb8c0f7756ca6b73c01935f136fb
earnesto <earnesto@107-16>
2024-10-21 14:37:34 +0200

    Agregar archivos de proyecto.

---
+++ AccesoBBDD/ListadoPersonas.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.ObjectModel;
+
+namespace AccesoBBDD
+{
+    // All the code in this file is included in all platforms.
+    public class ListadoPersonas<clsPersona>
+    {
+        public static List<clsPersona> getListadoPersonas() {
+            List<clsPersona> Personas = [
+                    new clsPersona () {nombre = "Pepe"})
+                ];
+
+            return personas;
+        }
+        
+    }
+}
---
+++ BibliotecaClases/clsPersona.cs
@@ -0,0 +1,8 @@
+﻿namespace BibliotecaClases
+{
+    // All the code in this file is included in all platforms.
+    public class clsPersona
+    {
+        String nombre { get; set; }
+    }
+}
